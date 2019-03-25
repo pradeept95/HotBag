9405b6a41e971252638ea420818bc308c0277feb
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-25 06:54:53 +0545

    application structure setup completed

--- HotBag.Application/Web.Host/Controllers/HomeController.cs
+++ HotBag.Application/Web.Host/Controllers/HomeController.cs
@@ -12,18 +12,7 @@ namespace Web.Host.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            return Redirect("/swagger");
+        } 
     }
 }
