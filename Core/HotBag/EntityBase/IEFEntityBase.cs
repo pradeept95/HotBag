80411761493204d0bce843894772a5b426d526e7
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-25 12:00:18 +0545

    Base Entity is Added

---
+++ Core/HotBag/EntityBase/IEFEntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace HotBag.EntityBase
+{
+    public interface IEFEntityBase<TPrimaryKey>
+    {
+        //
+        // Summary:
+        //     Unique identifier for this entity. 
+        TPrimaryKey Id { get; set; }
+    }
+      
+}
