bdb7fd9cdd8e08fa58682e339543621bf07802b5
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-04-09 21:30:43 +0545

    db context

--- Core/HotBag/EntityFramework/Context/HotBagDbContext.cs
+++ Core/HotBag/EntityFramework/Context/HotBagDbContext.cs
@@ -10,25 +10,7 @@ namespace HotBag.EntityFramework.Context
           : base(options)
         {
         }
-
-        #region Tanant Related Schema
-        public DbSet<Tenant> Tenant { get; set; }
-        public DbSet<TanentConfiguration> TanentConfiguration { get; set; }
-
-        #endregion
-
-        #region Tanant Related 
-        public DbSet<HotBagUser> HotBagUser { get; set; }
-        public DbSet<HotBagUserStatusLog> HotBagUserStatusLog { get; set; }
-        public DbSet<HotBagPasswordHistoryLog> HotBagPasswordHistoryLog { get; set; }
-        public DbSet<HotBagRole> HotBagRole { get; set; }
-        public DbSet<HotBagUserRoles> HotBagUserRoles { get; set; }
-        public DbSet<HotBagApplicationModule> HotBagApplicationModule { get; set; }
-        public DbSet<HotBagApplicationModulePermissionLevel> HotBagApplicationModulePermissionLevel { get; set; }
-        public DbSet<HotBagRoleApplicationModule> HotBagRoleApplicationModule { get; set; }
-
-        #endregion
-
+         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
              
--- Src/HotBag.EntityFrameworkCore/Context/ApplicationDbContext.cs
+++ Src/HotBag.EntityFrameworkCore/Context/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
-﻿using HotBag.Core.Entity;
+﻿using HotBag.AppUser;
+using HotBag.Core.Entity;
 using HotBag.EntityBase;
 using HotBag.EntityFramework.Context;
+using HotBag.Tanents;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotBag.EntityFrameworkCore.Context
@@ -11,6 +13,25 @@ namespace HotBag.EntityFrameworkCore.Context
           : base(options)
         {
         }
+
+        #region Tanant Related Schema
+        public DbSet<Tenant> Tenant { get; set; }
+        public DbSet<TanentConfiguration> TanentConfiguration { get; set; }
+
+        #endregion
+
+        #region Tanant Related 
+        public DbSet<HotBagUser> HotBagUser { get; set; }
+        public DbSet<HotBagUserStatusLog> HotBagUserStatusLog { get; set; }
+        public DbSet<HotBagPasswordHistoryLog> HotBagPasswordHistoryLog { get; set; }
+        public DbSet<HotBagRole> HotBagRole { get; set; }
+        public DbSet<HotBagUserRoles> HotBagUserRoles { get; set; }
+        public DbSet<HotBagApplicationModule> HotBagApplicationModule { get; set; }
+        public DbSet<HotBagApplicationModulePermissionLevel> HotBagApplicationModulePermissionLevel { get; set; }
+        public DbSet<HotBagRoleApplicationModule> HotBagRoleApplicationModule { get; set; }
+
+        #endregion
+
         public DbSet<TestEntity> TestEntity { get; set; }
         //public DbSet<Post> Posts { get; set; }
 
