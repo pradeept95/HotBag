083d3976df2e9d54ab2335e286e534730a082a84
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-04-11 08:04:12 +0545

    email module

--- Helpers/HotBag.Plugins.Email/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HotBag.Plugins.Email
-{
-    public class Class1
-    {
-    }
-}
---
+++ Helpers/HotBag.Plugins.Email/HotBagEmailModuel.cs
@@ -0,0 +1,37 @@
+﻿
+using HotBag.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotBag.Plugins.Email
+{
+    public class HotBagEmailModuel : ApplicationModule
+    {
+        public override string ModuleName
+        {
+            get { return "HotBagEmailModuel"; }
+
+        }
+
+        public override bool IsInstalled
+        {
+            get { return true; }
+
+        }
+
+        public override void Initialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            
+        }
+
+        public override void PostInitialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            
+        }
+
+        public override void PreInitialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            
+        }
+    }
+}
