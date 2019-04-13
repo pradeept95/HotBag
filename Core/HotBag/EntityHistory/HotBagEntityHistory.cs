f3c5384092296ad4c6420a6d33f1bde4abc86e01
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-04-13 16:36:05 +0545

    Entity history

---
+++ Core/HotBag/EntityHistory/HotBagEntityHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HotBag.EntityHistory
+{
+    public class HotBagEntityHistory
+    {
+        [Key]
+        public long Id { get; set; }
+        public string EntityName { get; set; }
+        public string PropertyName { get; set; }
+        public string PrimaryKeyValue { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime DateChanged { get; set; }
+    } 
+}
