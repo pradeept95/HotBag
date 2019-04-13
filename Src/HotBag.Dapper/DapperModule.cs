703629cfa8824fbb94815bb3883608bdfd47a83c
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-04-13 17:47:34 +0545

    Dapper Module Initiated

---
+++ Src/HotBag.Dapper/DapperModule.cs
@@ -0,0 +1,50 @@
+﻿using HotBag.DI.Base;
+using HotBag.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotBag.Dapper
+{
+    public class DapperModule : ApplicationModule
+    {
+        public override string ModuleName
+        {
+            get { return "DapperModule"; }
+
+        }
+
+        public override bool IsInstalled
+        {
+            get { return true; }
+
+        }
+
+        public override void Initialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+
+        } 
+
+        public override void PostInitialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            //SeedHelpers.SeedData(serviceCollection);
+        } 
+
+        public override void PreInitialize(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.Scan(scan => scan
+             .FromAssemblyOf<DapperModule>()
+                 .AddClasses(classes => classes.AssignableTo<ITransientDependencies>())
+                     .AsImplementedInterfaces()
+                     .WithTransientLifetime()
+
+                 .AddClasses(classes => classes.AssignableTo<IScopedDependencies>())
+                     .AsImplementedInterfaces()
+                     .WithScopedLifetime()
+
+                  .AddClasses(classes => classes.AssignableTo<ISingletonDependencies>())
+                             .AsImplementedInterfaces()
+                             .WithSingletonLifetime());
+
+        } 
+    }
+}
