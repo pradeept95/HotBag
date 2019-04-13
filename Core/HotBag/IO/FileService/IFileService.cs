a733d4b62d7501cf49722bb666f7310dce73715d
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-04-13 07:01:09 +0545

    File Service Added

---
+++ Core/HotBag/IO/FileService/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.IO.FileServices
+{
+    public interface IFileService
+    {
+        string Save(IFormFile file);
+        string Save(string dirPath, IFormFile file);  
+        void Delete(string dirPath, string filePath);
+        string getRelativePath(string dirPath, string filePath);
+    }
+}
